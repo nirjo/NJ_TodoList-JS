5bdf04ade4c3dc8561ce1187c5c5553ecf9f8d29
Nirmalraj Joseph <nirmalraj.joseph86@gmail.com>
2020-10-03 20:11:38 +0530

    first commit

--- app.js
+++ app.js
@@ -40,6 +40,12 @@ function addTodo(e) {
   todoDiv.appendChild(trashButton);
   //attach final Todo
   todoList.appendChild(todoDiv);
+  // Add Logo 
+  const logo = document.querySelectorAll("#logo path");
+
+  for (let i=0; i< logo.length; i++) {
+    console.log('Letter ${i} is $ {logo[i].getTotalLength()}');
+  }
 }
 
 function deleteTodo(e) {
